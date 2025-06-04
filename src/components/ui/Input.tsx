8ef534ab8884dae801c8a6513fdb705456356df3
MarcioMFS <73888199+MarcioMFS@users.noreply.github.com>
2025-06-04 20:37:22 -0300

    Fix input icons positioning

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -37,7 +37,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
         )}
         <div className="relative">
           {leftIcon && (
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-gray-500">
               {leftIcon}
             </div>
           )}
@@ -55,7 +55,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
             {...props}
           />
           {rightIcon && (
-            <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+            <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none text-gray-500">
               {rightIcon}
             </div>
           )}
