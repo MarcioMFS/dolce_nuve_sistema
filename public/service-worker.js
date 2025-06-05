807d230ad262e5fec23a13873b4b988c0a679890
MarcioMFS <73888199+MarcioMFS@users.noreply.github.com>
2025-06-05 11:44:42 -0300

    Updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,6 +32,6 @@ self.addEventListener('fetch', event => {
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
         return response;
       });
-    }).catch(() => caches.match('/'))
+    }).catch(() => caches.match('/index.html'))
   );
 });
